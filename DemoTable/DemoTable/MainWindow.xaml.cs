e273e6031bcc43c26213d629e075fc133141cfa8
Jakob Ladegaard Møller <jako6440@edu.campusvejle.dk>
2018-02-28 14:51:59 +0100

    Initial commit

---
+++ DemoTable/DemoTable/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace DemoTable
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private DispatcherTimer countDown = new DispatcherTimer();
+
+		public delegate void GameEndEventHandler(object sender, EventArgs e);
+		public event GameEndEventHandler GameEnd;
+		private List<Player> players = new List<Player>();
+
+		public double timer = 10;
+		public MainWindow()
+		{
+			for(int i = 0; i < 4; i++)
+				players.Add(new Player(this));
+			AddChild(players[0]);
+			InitializeComponent();
+			countDown.Interval = new TimeSpan(0,0,0,0,1);
+			countDown.Tick += CountDown_Tick;
+		}
+		private void CountDown_Tick(object sender, EventArgs e)
+		{
+			if(timer > 0)
+			{
+				timer -= 0.001;
+			}
+			else
+			{
+				countDown.Stop();
+				GameEnd.Invoke(this, new EventArgs());
+				timer = 10;
+			}
+		}
+	}
+}
---
+++ DemoTable/DemoTable/Player.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace DemoTable
+{
+	/// <summary>
+	/// Interaction logic for Player.xaml
+	/// </summary>
+	public partial class Player : Page, INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+		public Player(MainWindow mainwindow)
+		{
+			InitializeComponent();
+			LabelScore.DataContext = this;
+			mainwindow.GameEnd += Reset;
+		}
+
+		private int score = 0;
+		public int Score
+		{
+			get => score;
+			set
+			{
+				score = value;
+				OnPropertyChanged("Score");
+			}
+		}
+
+		private void ButtonScoreIncrease_Click(object sender, RoutedEventArgs e) => Score++;
+
+		private void Reset(object sender, EventArgs e) => Score = 0;
+
+		private void OnPropertyChanged(string property) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+
+		private void ButtonTimer_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+	}
+}
